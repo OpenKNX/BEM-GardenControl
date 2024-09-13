4152508f8089483f9f01cbd0efc94fa314ac9512
Matthias <m.freudenreich@smart-mf.de>
2024-09-13 09:50:24 +0200

    first commit GardenControl 2.0

--- include/versions.h
+++ include/versions.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#define MAIN_Version "d8f6ab0"
-#define KNX_Version "9d7c8ac"
-#define MODULE_Common_Version "1.1.1+7d5ea92"
-#define MODULE_Common_ETS 17
-#define MODULE_LogicModule_Version "3.1.4+cbc8707"
-#define MODULE_LogicModule_ETS 49
+#define MAIN_Version "b661875"
+#define KNX_Version "2.1.0+84a0ce3"
+#define MODULE_Common_Version "1.2.0+8410b2a"
+#define MODULE_Common_ETS 18
+#define MODULE_LogicModule_Version "3.3.1+9335c33"
+#define MODULE_LogicModule_ETS 51
 #define MODULE_FileTransferModule_Version "0.0.4+0ba2086"
 #define MODULE_FileTransferModule_ETS 0
