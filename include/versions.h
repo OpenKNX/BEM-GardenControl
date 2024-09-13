f131ee32211db96e75cf73418d1e9018eaedb8e2
Matthias <m.freudenreich@smart-mf.de>
2024-09-13 11:10:24 +0200

    update Modulversion, compily success

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAIN_Version "b661875"
+#define MAIN_Version "4152508"
 #define KNX_Version "2.1.0+84a0ce3"
 #define MODULE_Common_Version "1.2.0+8410b2a"
 #define MODULE_Common_ETS 18
